b478e279344fd6325659ab43f6aeeb62ade3316c
Valentin <vh64g@gmx.de>
2022-08-11 21:54:03 +0200

    worked on the login screen
    Signed-off-by: Valentin <vh64g@gmx.de>

--- app/src/main/java/com/example/social_network/login.kt
+++ app/src/main/java/com/example/social_network/login.kt
@@ -1,11 +1,33 @@
 package com.example.social_network
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdLoader
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.nativead.NativeAd
+import com.google.android.gms.ads.nativead.NativeAdOptions
 
 class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        MobileAds.initialize(this) {}
+        val adLoader = AdLoader.Builder(this, "ca-app-pub-1839648225801792/8563792553")
+            .forNativeAd { ad : NativeAd ->
+                // Show the ad.
+            }
+            .withAdListener(object : AdListener() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    // Handle the failure by logging, altering the UI, and so on.
+                }
+            })
+            .withNativeAdOptions(
+                NativeAdOptions.Builder()
+                // Methods in the NativeAdOptions.Builder class can be
+                // used here to specify individual options settings.
+                .build())
+            .build()
     }
 }
