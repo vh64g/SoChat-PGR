400a79d37f5c4c94a4adc26350e481ce3057d2a2
Valentin <vh64g@gmx.de>
2022-08-12 12:16:25 +0200

    added functionality of ads, worked on the design
    Signed-off-by: Valentin <vh64g@gmx.de>

--- app/src/main/java/com/example/social_network/login.kt
+++ app/src/main/java/com/example/social_network/login.kt
@@ -1,33 +1,36 @@
 package com.example.social_network
+
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.ads.AdListener
+import com.google.android.ads.nativetemplates.NativeTemplateStyle
+import com.google.android.ads.nativetemplates.TemplateView
 import com.google.android.gms.ads.AdLoader
-import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.nativead.NativeAd
-import com.google.android.gms.ads.nativead.NativeAdOptions
+
+
+const val ADMOB_AD_UNIT_ID = "ca-app-pub-1839648225801792/8563792553"
 
 class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         MobileAds.initialize(this) {}
-        val adLoader = AdLoader.Builder(this, "ca-app-pub-1839648225801792/8563792553")
-            .forNativeAd { ad : NativeAd ->
-                // Show the ad.
+
+        val adLoader = AdLoader.Builder(this, ADMOB_AD_UNIT_ID)
+            .forNativeAd { nativeAd ->
+                val background = ColorDrawable(-0x999a)
+                val styles: NativeTemplateStyle =
+                    com.google.android.ads.nativetemplates.NativeTemplateStyle.Builder()
+                        .withMainBackgroundColor(background).build()
+                val template: TemplateView = findViewById<TemplateView>(R.id.my_template)
+                template.setStyles(styles)
+                template.setNativeAd(nativeAd)
             }
-            .withAdListener(object : AdListener() {
-                override fun onAdFailedToLoad(adError: LoadAdError) {
-                    // Handle the failure by logging, altering the UI, and so on.
-                }
-            })
-            .withNativeAdOptions(
-                NativeAdOptions.Builder()
-                // Methods in the NativeAdOptions.Builder class can be
-                // used here to specify individual options settings.
-                .build())
             .build()
+
+        adLoader.loadAd(AdRequest.Builder().build())
+
     }
 }
